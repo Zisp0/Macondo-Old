cc769775040af1cc2fe335bea09eade15b22600d
Zisp0 <92941421+Zisp0@users.noreply.github.com>
2021-12-08 09:42:36 -0500

    Add files via upload

--- vista/js/publicaciones.js
+++ vista/js/publicaciones.js
@@ -83,7 +83,7 @@ function validarPost() {
 
 function publicarPost() {
     let hoy = new Date();
-    let fecha = hoy.getDate() + '/' + ( hoy.getMonth() + 1 ) + '/' + hoy.getFullYear();
+    let fecha = hoy.getFullYear() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getDate();
     let hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
 
     $.ajax({
