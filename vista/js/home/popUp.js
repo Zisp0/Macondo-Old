a8a9612738e0b0a1f96fb83fee520fa4ddbb94fa
Zisp0 <92941421+Zisp0@users.noreply.github.com>
2021-12-08 09:08:32 -0500

    Delete vista/js/home directory

--- vista/js/home/popUp.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".pop-up").hide();
-    $("#buttonInputPost").click(function () {
-        $(".pop-up").show();
-    });  
-    $("#buttonCerrarModal").click(function () {
-        $(".pop-up").hide();
-    });
-});
