dc30a11205c851b09379a27f35e8c3ebb5f0e4c8
Wilkar Marquez <wilkarmarquez13@gmail.com>
2021-12-08 19:26:10 -0500

    fase inicial de btones like dislike y comentar

--- controlador/accion/act_cargarPublicaciones.php
+++ controlador/accion/act_cargarPublicaciones.php
@@ -11,8 +11,23 @@
                 
             $resultado .= '
             <div class="post">
+                <div class="ContainerPicturesNameUser">
+                    <div><img src="pictures/defaultPictureUser.png"></div>               
+                    <p>Nombre User</p>
+                </div>
                 <h3>'.$publicaciones[$indice]->getTitulo().'</h3>
                 <article>'.$publicaciones[$indice]->getContenido().'</article>
+                <div class="containerReactionComment">
+                    <div class="containerLike">
+                        <button id="buttonLike"><img src="pictures/reactionPictures.png"/><p>Me gusta</p></button>
+                    </div>
+                    <div class="containerDislike">
+                        <button id="buttonDislike"><img src="pictures/reactionPictures.png"/><p>No me gusta</p></button>
+                    </div>
+                    <div class="containerComment">
+                        <button id="buttonComment"><img src="pictures/reactionPictures.png"/><p>Comentar</p></button>
+                    </div>
+                </div>
             </div>
             ';
         } 
